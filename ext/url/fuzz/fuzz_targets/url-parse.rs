fde0471e2112f3e48577e688fe9dbbd04bd73d3c
Evan Richter <evanjrichter@gmail.com>
2022-09-17 16:41:16 -0500

    update fuzz harness

--- ext/url/fuzz/fuzz_targets/url-parse.rs
+++ ext/url/fuzz/fuzz_targets/url-parse.rs
@@ -1,22 +1,29 @@
 #![no_main]
 
+use deno_core::OpState;
 use libfuzzer_sys::fuzz_target;
 
 #[derive(Debug, arbitrary::Arbitrary)]
 enum Target {
-  UrlParse(String, Option<String>),
-  UrlReparse(String, (u8, String)),
+  UrlParse(String),
+  UrlReparse(String, u8, String),
   UrlParseSearchParams(Option<String>, Option<Vec<u8>>),
   UrlStringifySearchParams(Vec<(String, String)>),
 }
 
 fuzz_target!(|target: Target| {
   match target {
-    Target::UrlParse(url, base) => {
-      let _ = deno_url::op_url_parse::call(url, base);
+    Target::UrlParse(url) => {
+      let mut op = OpState::new(64);
+      let mut buf = [0; 32];
+      let _ = deno_url::op_url_parse::call(&mut op, url, &mut buf);
     }
-    Target::UrlReparse(url, opts) => {
-      let _ = deno_url::op_url_reparse::call(url, opts);
+    Target::UrlReparse(url, setter, setter_val) => {
+      let mut op = OpState::new(64);
+      let mut buf = [0; 32];
+      let _ = deno_url::op_url_reparse::call(
+        &mut op, url, setter, setter_val, &mut buf,
+      );
     }
     Target::UrlParseSearchParams(args, buf) => {
       let zc = buf.map(serde_v8::ZeroCopyBuf::new_temp);
